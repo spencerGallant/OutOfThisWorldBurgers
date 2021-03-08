8101da415f778812f7576e7ce5693b87ca06357b
sgallant <sgallant@Spencers-MacBook-Pro.local>
2021-03-07 19:12:56 -0500

    tilemap, cows, pillars, meat grinder

---
+++ GameDesignMidterm/Assets/InitialCowMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialCowMovement : MonoBehaviour
+{
+	public Rigidbody2D rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		int dir = Random.Range(0, 2);
+		if(dir == 0) rb.AddForce(new Vector2(Random.Range(300, 500), Random.Range(300, 500)));
+		else rb.AddForce(new Vector2(Random.Range(-300, -500), Random.Range(-300, -500)));
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+
+    }
+}
